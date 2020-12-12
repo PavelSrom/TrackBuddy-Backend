80b84a46c00452fa575593066e1b631bfb8a2efa
Pavel Srom <srompavel98@gmail.com>
2020-12-12 09:11:09 +0100

    fix(auth): fix get token secret

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,19 +1,16 @@
-import config from 'config'
 import jwt from 'jsonwebtoken'
 import { Req, Res, Next, Token } from '../utils/generic-types'
+import { getKey } from '../utils/get-key'
 
 export default (req: Req, res: Res, next: Next) => {
   const token = req.header('trackbuddy-token')
   if (!token) return res.status(401).send({ message: 'Missing token' })
 
   try {
-    const decoded = jwt.verify(token, config.get('jwtSecret')) as Token
+    const decoded = jwt.verify(token, getKey('JWT_SECRET')) as Token
     req.userId = decoded.id
 
-    // TODO: remove this timeout before release
-    setTimeout(() => {
-      next()
-    }, 1000)
+    next()
   } catch (err) {
     console.log(err)
     return res.status(401).send({ message: 'Invalid token' })
